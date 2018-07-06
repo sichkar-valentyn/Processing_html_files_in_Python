bbc0b102f4181c362e2a3fab0189a9886a43e4ca
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-07-06 07:42:24 +0300

    Update html_format.py

--- html_format.py
+++ html_format.py
@@ -1,3 +1,16 @@
+# File: html_format.py
+# Description: Examples on how to process html files in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Examples on how to process html files in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Processing_html_files_in_Python (date of access: XX.XX.XXXX)
+
+
+
 # Working with files in html format
 
 # Importing library lxml
